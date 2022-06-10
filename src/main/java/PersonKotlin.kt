c9950fe6b8d28f82489508e0d76c21de423b5fd0
Nikita <l.r.shiun@gmail.com>
2022-06-10 12:55:47 +0800

    Kotlin hello

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,14 @@
+package com.nikita
+
+
+class PersonKotlin {
+    fun greeting(){
+        println("First Kotlin")
+    }
+}
+
+//1st level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
